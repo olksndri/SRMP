4e7c4243ac98ca94137759d836dfd0cd819f9314
Oleksandr Tympshenko <hermeusmanua@gmail.com>
2024-02-04 03:54:06 +0200

    Created PRJ
    Created simple Student Records Management program (CLI app)

---
+++ Src/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+typedef struct{
+	int32_t 	record_number;
+	char 		name[100];
+	char 		branch[50];
+	char 		dob[15];
+	int32_t 	semester;
+}STUDENT_INFO_t;
+
+#endif
